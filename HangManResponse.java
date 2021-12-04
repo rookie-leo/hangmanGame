09caf5c5625a44ca73ee208d4e0c37f7abc09438
rookie-leo <proleonarpa@gmail.com>
2021-12-04 11:28:56 -0300

    Configuracao inicial do projeto

---
+++ HangManResponse.java
@@ -0,0 +1,10 @@
+HangManResponse :
+
+	private String result;
+	private String hangMan;
+	private Integer maxAttempts;
+	private Integer wrongAttempts;
+	private Boolean correctLetter;
+	private Boolean gameOver;
+	private Set<Character> listAttempt;
+	private String msg;
