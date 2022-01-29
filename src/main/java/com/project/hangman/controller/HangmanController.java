06268fd0ef5cc4276094251019f8a4236577e0bb
rookie-leo <proleonarpa@gmail.com>
2022-01-29 17:43:17 -0300

    Elaborando a logica do jogo

---
+++ src/main/java/com/project/hangman/controller/HangmanController.java
@@ -0,0 +1,12 @@
+package com.project.hangman.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hangman")
+public class HangmanController {
+
+	
+	
+}
---
+++ src/main/java/com/project/hangman/service/PartidaService.java
@@ -0,0 +1,20 @@
+package com.project.hangman.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.project.hangman.model.Palavra;
+
+@Service
+public class PartidaService {
+
+	public void partida(List<Jogador> jogadores) {
+		
+		while (true) {
+			for (int i = 0; jogadores.size(); i++) {
+			}
+		}
+	}
+	
+}
