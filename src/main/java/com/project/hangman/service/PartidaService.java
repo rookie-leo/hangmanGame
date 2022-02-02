2959442a493371907303b781f241a388fd355711
rookie-leo <proleonarpa@gmail.com>
2022-02-01 21:57:04 -0300

    Implementando logica da partida

--- src/main/java/com/project/hangman/service/PartidaService.java
+++ src/main/java/com/project/hangman/service/PartidaService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.project.hangman.model.Jogador;
 import com.project.hangman.model.Palavra;
 
 @Service
