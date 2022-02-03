4ff249664c3b637ddd9b4c3452c2e61c943480fb
rookie-leo <proleonarpa@gmail.com>
2022-02-02 21:09:38 -0300

    Implementado metodo para trabalhar a palavra para o jogo

--- src/main/java/com/project/hangman/service/HangmanService.java
+++ src/main/java/com/project/hangman/service/HangmanService.java
@@ -9,21 +9,30 @@ import com.project.hangman.model.Jogador;
 import com.project.hangman.model.Palavra;
 
 @Service
-public class PartidaService {
+public class HangmanService {
 
 	@Autowired
 	private PalavraService pService;
 	
 	public void partida(List<Jogador> jogadores) {
+
+		
+//		
+//		while (true) {
+//			for (int rodada = 0; jogadores.size(); rodada++) {
+//			}
+//		}
+	}
+	
+	/**
+	 * @method escolhe uma palavra aleatoriamente no banco de dados e transforma ela em um array de caracteres '_'
+	 * @return retorna um array array de caracteres '_'
+	 * */
+	public char[] trabalhaPalavra() {
 		Palavra palavra = pService.escolherPalavra();
 		char[] conjunto = pService.desmontarPalavra(palavra.getPalavra());
 		char[] icognito = pService.escondePalavra(conjunto);
 		
-		
-		while (true) {
-			for (int rodada = 0; jogadores.size(); rodada++) {
-			}
-		}
+		return icognito;
 	}
-	
 }
