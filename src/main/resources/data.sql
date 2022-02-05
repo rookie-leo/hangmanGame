3d11528f27f079d84f2598615e9b75ef3ca06766
rookie-leo <proleonarpa@gmail.com>
2022-02-05 12:45:22 -0300

    Arquivo data.sql criado com algumas palavras cadastradas

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO palavra(palavra, tamanho_palavra) values('Testando', 8);
+INSERT INTO palavra_dicas(palavra_id, dicas) values(1, 'Testando data.sql');
+INSERT INTO palavra(palavra, tamanho_palavra) values('RAP', 3);
+INSERT INTO palavra_dicas(palavra_id, dicas) values(2, 'Estilo musical');
+INSERT INTO palavra(palavra, tamanho_palavra) values('God of War', 8);
+INSERT INTO palavra_dicas(palavra_id, dicas) values(3, 'Jogo envolvendo mitologia');
+INSERT INTO palavra(palavra, tamanho_palavra) values('Batman', 6);
+INSERT INTO palavra_dicas(palavra_id, dicas) values(4, 'Heroi de historias em quadrinhos');
+INSERT INTO palavra(palavra, tamanho_palavra) values('Java', 4);
+INSERT INTO palavra_dicas(palavra_id, dicas) values(5, 'Melhor linguagem de programação ever');
+INSERT INTO palavra(palavra, tamanho_palavra) values('Python', 6);
+INSERT INTO palavra_dicas(palavra_id, dicas) values(6, 'Segunda melhor linguagem de programação');
